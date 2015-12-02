efa99c34896b60c85edbd5e1a010fbff4513817b
flankstaek <iswain@ccs.neu.edu>
2015-12-02 15:52:12 -0500

    removed dummy response

--- Steam/cloud/user.js
+++ Steam/cloud/user.js
@@ -21,20 +21,6 @@ module.exports.get = function(urlParams, response) {
       //failure
       response.error("incorrect vanity url");
     });
-
-
-
-    var user = {
-        "id": 1,
-        "steam_id": 123,
-        "display_name": "Petracles",
-        "avatar_small": "www.url.com",
-        "avatar_medium": "www.url.com",
-        "avatar_full": "www.url.com",
-        "created_at": "2015-11-16T22:23:48Z",
-        "updated_at": "2015-11-16T22:23:48Z"
-    };
-    response.success(user);
 };
 
 module.exports.create = function(body, response) {
