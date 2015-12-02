1e0739a9d64a9ba9a2c8913e58da01775656c67c
akrischer <krischer.a@husky.neu.edu>
2015-12-02 13:36:30 -0500

    implement GET Criteria

--- Steam/cloud/criterion.js
+++ Steam/cloud/criterion.js
@@ -1,38 +1,51 @@
 var images = require('cloud/utils/images.js');
+var respond = require('cloud/utils/respond.js');
+
+var Criterion = Parse.Object.extend('Criterion');
+
+var stubbed_criteria = [
+    {
+        "id": 1,
+        "tags": [
+            {
+                "id": 1,
+                "display_tag": "short game",
+                "hidden_tag": "short"
+            },
+            {
+                "id": 3,
+                "display_tag": "casual",
+                "hidden_tag": "casual"
+            }
+        ],
+        "name": "Time",
+        "icon_url": getImageUrl("Time")
+    },
+    {
+        "id": 2,
+        "tags": [
+            {
+                "id": 2,
+                "display_tag": "Multiplayer",
+                "hidden_tag": "co-op"
+            }
+        ],
+        "name": "Co-Op",
+        "icon_url": getImageUrl("Co-Op")
+    }
+];
 
 module.exports.get = function(urlParams, response) {
-    var criteria = [
-        {
-            "id": 1,
-            "tags": [
-                {
-                    "id": 1,
-                    "display_tag": "short game",
-                    "hidden_tag": "short"
-                },
-                {
-                    "id": 3,
-                    "display_tag": "casual",
-                    "hidden_tag": "casual"
-                }
-            ],
-            "name": "Time",
-            "icon_url": getImageUrl("Time")
-        },
-        {
-            "id": 2,
-            "tags": [
-                {
-                    "id": 2,
-                    "display_tag": "Multiplayer",
-                    "hidden_tag": "co-op"
-                }
-            ],
-            "name": "Co-Op",
-            "icon_url": getImageUrl("Co-Op")
-        }
-    ];
-    response.success(criteria);
+    var query = new Parse.Query(Criterion);
+    query.include('tags');
+    query.equalTo("enabled", true);
+    query.descending('createdAt');
+
+    query.find().then(function(resources) {
+        respond.success(response, resources)
+    }, function(error) {
+        response.error(error);
+    });
 };
 
 function getImageUrl(criterionName) {
