4eeb90410ebb8f0af1ff8c08477852642ab0bbf9
akrischer <krischer.a@husky.neu.edu>
2015-12-02 15:14:40 -0500

    add some debugging

--- Steam/cloud/question.js
+++ Steam/cloud/question.js
@@ -1,5 +1,6 @@
 var respond = require('cloud/utils/respond.js');
 var parseUtils = require('cloud/utils/parseUtils.js');
+var _ = require('underscore');
 
 var Question = Parse.Object.extend('Question');
 
@@ -7,6 +8,11 @@ module.exports.get = function(urlParams, response) {
     var includeOnly = urlParams.include_only ? urlParams.include_only : [];
     // map includeOnly to become array of pointers to Criterion
     includeOnly = parseUtils.createListOfPointers("Criterion", includeOnly);
+
+    console.log("~-~-~- Include_Only pointers: ~-~-~-");
+    _.each(includeOnly, function(ptr) {
+        console.log(ptr.id);
+    });
 
     var query = new Parse.Query(Question);
     query.include('tag');
