2fad2f4382de18c2965a4fe68a4218e55ed6ce25
akrischer <krischer.a@husky.neu.edu>
2015-12-02 14:56:25 -0500

    execute query for questions...

--- Steam/cloud/question.js
+++ Steam/cloud/question.js
@@ -3,52 +3,6 @@ var parseUtils = require('cloud/utils/parseUtils.js');
 
 var Question = Parse.Object.extend('Question');
 
-var questions = [
-    {
-        "criterion_id": 1,
-        "questions": [
-            {
-                "id": 1,
-                "tag": {
-                    "id": 1,
-                    "display_tag": "short game",
-                    "hidden_tag": "short"
-                },
-                "text": "Do you want to play a short game?",
-                "responses": [
-                    {
-                        "text": "Yes",
-                        "valence": 1
-                    },
-                    {
-                        "text": "No",
-                        "valence": -1
-                    }
-                ]
-            },
-            {
-                "id": 2,
-                "tag": {
-                    "id": 1,
-                    "display_tag": "short game",
-                    "hidden_tag": "short"
-                },
-                "text": "Do you LIKE shorter games?",
-                "responses": [
-                    {
-                        "text": "Yes",
-                        "valence": 1
-                    },
-                    {
-                        "text": "No",
-                        "valence": -1
-                    }
-                ]
-            }
-        ]
-    }
-];
-
 module.exports.get = function(urlParams, response) {
     var includeOnly = urlParams.include_only | [];
     // map includeOnly to become array of pointers to Criterion
@@ -61,5 +15,10 @@ module.exports.get = function(urlParams, response) {
     if (includeOnly.length > 0) {
         query.containedIn('criterion', includeOnly);
     }
-    respond.success(response, questions);
+
+    query.find().then(function(questions) {
+        respond.success(response, questions);
+    }, function(error) {
+        response.error(error);
+    });
 };
